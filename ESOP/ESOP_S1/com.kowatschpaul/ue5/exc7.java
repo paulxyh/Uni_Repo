fd80af06722c0c3b7febb934f07e50308f77fcb1
paulxyh <k.paulxyh@gmail.com>
2022-11-16 18:51:29 +0100

    exc7 final

--- ESOP/ESOP_S1/com.kowatschpaul/ue5/exc7.java
+++ ESOP/ESOP_S1/com.kowatschpaul/ue5/exc7.java
@@ -1,5 +1,18 @@
 package com.kowatschpaul.ue5;
 
 public class exc7 {
-   static int[]
+   static int[] addArrayValues(int[] arr1, int[] arr2){
+      if(arr1 != null && arr2 != null && arr1.length == arr2.length){
+         int[] arr_Result = new int[arr1.length];
+         for(int i = 0; i < arr_Result.length; i++){
+            arr_Result[i] = arr1[i] + arr2[i];
+         }
+         return arr_Result;
+      }
+      else {
+         int[] arr_empty = new int[0];
+         return arr_empty;
+      }
+
+   }
 }
