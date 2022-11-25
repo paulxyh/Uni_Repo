3729c57c106573d51e1393642a13587dabe93d20
paulxyh <77801088+paulxyh@users.noreply.github.com>
2022-11-25 09:54:45 +0100

    ue6 finalisiert

--- ESOP/ESOP_S1/com.kowatschpaul/ue6/exc11.java
+++ ESOP/ESOP_S1/com.kowatschpaul/ue6/exc11.java
@@ -2,7 +2,43 @@ package com.kowatschpaul.ue6;
 
 public class exc11 {
     public static void main(String[] args){
-        int size = 3;
+        int size = 7;
+        char[][] hourglass = createHourglass(size);
+
+        // print the output
+        for(char[] row: hourglass){
+            for(char ch: row){
+                System.out.print(ch);
+            }
+            System.out.print("\n");
+        }
+    }
+
+    // TODO Define your method here
+    static char[][] createHourglass(int size){
+        char[][] result = new char[size][size];
+        for(int i = 0; i < size; i++){
+            for(int k = 0; k < size; k++){
+                if(k >= i && k <= size - i - 1){
+                    result[i][k] = '*';
+                }
+                else{
+                    result[i][k] = ' ';
+                }
+            }
+        }
+        for(int i = 0; i < size; i++){
+            for(int k = 0; k < size; k++){
+                if(k >= i && k <= size-i-1){
+                    result[size-i-1][k] = '*';
+                }
+            }
+        }
+        return result;
+    }
+    /*
+    * public static void main(String[] args){
+        int size = 5;
         char[][] hourglass = createHourglass(size);
 
         // print the output
@@ -20,7 +56,7 @@ public class exc11 {
         int split = size / 2;
         for(int i = 0; i < size; i++){
             for(int k = 0; k < size; k++){
-                if(k >= i && k <= size - i - 1){
+                if(k >= i && k <= size - 1 - i){
                     result[i][k] = '*';
                 }
                 else{
@@ -29,12 +65,12 @@ public class exc11 {
             }
         }
         for(int i = 0; i < split; i++){
-            for(int k = 1; k < size -i; k++){
+            for(int k = i; k < size -i; k++){
                 if(k >= i && k <= size-i){
                     result[size-i-1][k] = '*';
                 }
             }
         }
         return result;
-    }
+    }*/
 }
