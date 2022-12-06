301bbea176daed80b06f5a66161ce9ff66239961
paulxyh <77801088+paulxyh@users.noreply.github.com>
2022-12-06 18:25:02 +0100

    VO ESOP & VO DM & BSP 40 DM

--- ESOP/ESOP_S1/com.kowatschpaul/vo/lecture5_12/Person.java
+++ ESOP/ESOP_S1/com.kowatschpaul/vo/lecture5_12/Person.java
@@ -1,2 +1,22 @@
-package com.kowatschpaul.vo.lecture5_12;public class Person {
+package com.kowatschpaul.vo.lecture5_12;
+
+public class Person {
+    String firstname = "--";
+    String lastname =  "--";
+    boolean isStudent = false;
+
+    public Person(String first, String lastname){
+        firstname = first;
+        this.lastname =  lastname;
+        System.out.print("Aufruf beim Erzeugen von Person mit: ");
+        this.printName();
+    }
+
+    public boolean isEqual(Person other){
+        return this.firstname.equals(other.firstname) && this.lastname.equals(other.lastname);
+    }
+
+    public void printName(){
+        System.out.println(this.firstname + " " + this.lastname);
+    }
 }
--- ESOP/ESOP_S1/com.kowatschpaul/vo/lecture5_12/TestPerson.java
+++ ESOP/ESOP_S1/com.kowatschpaul/vo/lecture5_12/TestPerson.java
@@ -1,2 +1,24 @@
-package com.kowatschpaul.vo.lecture5_12;public class TestPerson {
+package com.kowatschpaul.vo.lecture5_12;
+import com.kowatschpaul.vo.lecture5_12.Person;
+
+
+public class TestPerson {
+    public static void main(String[] args) {
+        Person p1 = new Person("Paul", "Kowatsch");
+
+        Person p2 = new Person("Eva", "Moser");
+        p1.firstname = "Max";
+        p1.printName();
+        p2.printName();
+
+        if(p1.isEqual(p2))
+            System.out.println("Gleich");
+        else
+            System.out.println("Ungleich");
+
+
+        System.out.println("Adresse: " + p1);
+        System.out.println("Adresse: " + p2);
+
+    }
 }
